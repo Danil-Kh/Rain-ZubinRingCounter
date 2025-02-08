f5d0a87d8a4efdee58183f5041fb9a7d7f6c5cbc
Danil <dabilsch60@gmail.com>
2025-02-08 22:56:25 +0200

    Create project

---
+++ src/main/java/org/example/rainzubinringcounter/RainZubinRingCounterApplication.java
@@ -0,0 +1,13 @@
+package org.example.rainzubinringcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainZubinRingCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainZubinRingCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rainzubinringcounter/RainZubinRingCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rainzubinringcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainZubinRingCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
