2647dbeabe60dbf85b749254cc830761fc9c6222
Danil <dabilsch60@gmail.com>
2025-02-12 16:05:27 +0200

    added output of the number of rings to the frontend also added generation of a .docx file

--- src/main/java/org/example/rainzubinringcounter/RingCounterController.java
+++ src/main/java/org/example/rainzubinringcounter/RingCounterController.java
@@ -2,24 +2,33 @@ package org.example.rainzubinringcounter;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.input.Clipboard;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import org.springframework.stereotype.Component;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 public class RingCounterController {
+    @FXML
+    private TextArea textArea;
+    private RingReader ringReader = new RingReader();
     @FXML
     public Circle circleDrag;
     private final FileChooser fileChooser = new FileChooser();
     @FXML
     public Button fileChooserButton;
+    private final XWPFDocument xwpfDocument = new XWPFDocument();
 
     @FXML
     private void initialize() {
@@ -49,10 +58,28 @@ public class RingCounterController {
             if (db.hasFiles()) {
                 success = true;
                 for (File file : db.getFiles()) {
+                    StringBuilder sb = new StringBuilder();
                     System.out.println("Dropped file: " + file.getAbsolutePath());
+                    HashMap<String, Integer> hashMap = ringReader.reader(file.getAbsolutePath());
+                    for (String key : hashMap.keySet()) {
+                        sb.append(key).append(": ").append(hashMap.get(key)).append("\n");
+                    }
+                    textArea.appendText(sb.toString());
+                    try {
+                        FileOutputStream fileOutputStream = new FileOutputStream("test.docx");
+                        XWPFRun run = xwpfDocument.createParagraph().createRun();
+                        run.setText(sb.toString());
+                        xwpfDocument.write(fileOutputStream);
+
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
                 }
             }
-
             event.setDropCompleted(success);
             event.consume();
         });
