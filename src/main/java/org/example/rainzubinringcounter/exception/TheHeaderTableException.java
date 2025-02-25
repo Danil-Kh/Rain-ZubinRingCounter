dba8d5a0884d27b8212923fe1d616c2c45dc42b7
nikita <nikita.ziubin@gmail.com>
2025-02-26 00:20:52 +0200

    Add error occurrence when the table header is not written properly.

---
+++ src/main/java/org/example/rainzubinringcounter/exception/TheHeaderTableException.java
@@ -0,0 +1,7 @@
+package org.example.rainzubinringcounter.exception;
+
+public class TheHeaderTableExeption extends RuntimeException {
+    public TheHeaderTableExeption(ExceptionMessage message) {
+        super(message);
+    }
+}
