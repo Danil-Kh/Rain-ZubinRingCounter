64a5e3a6427d42f7e4f9e25c41b3a730a2841470
nikita <nikita.ziubin@gmail.com>
2025-08-09 14:25:35 +0300

    Made the refactor to the class RingReader.

--- src/main/java/org/example/rainzubinringcounter/RingReader.java
+++ src/main/java/org/example/rainzubinringcounter/RingReader.java
@@ -15,15 +15,13 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 @Slf4j
 @Service
 public class RingReader {
-
     private static final Integer MIN_WORDS_TO_RING = 2;
     private List<Ring> ringList;
-    private final Collator ukrLang = Collator.getInstance(new Locale("uk", "UA"));
-    public Map<String, Integer> sortedHashMap = new TreeMap<>(ukrLang);
+    private final Collator ukrLang = Collator.getInstance(Locale.of("uk", "UA"));
+    public final Map<String, Integer> sortedHashMap = new TreeMap<>(ukrLang);
     public final List<String> errorsList = new ArrayList<>();
     private final Map<String, List<String>> nameToTimes = new HashMap<>();
     private final GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
@@ -36,24 +34,25 @@ public class RingReader {
         }
 
         try (FileInputStream fis = new FileInputStream(docxPath);
-            XWPFDocument document = new XWPFDocument(fis)) {
-            if (document.getTables().isEmpty()) {
-//                log.info("==============Deal with paragraphs==============");
-                dealWithRingsInParagraph(document);
-            } else {
-//                log.info("==============Deal with tables==============");
-                dealWithRingsInTable(document);
+            XWPFDocument originalDocument = new XWPFDocument(fis)) {
+            if (originalDocument.getTables().isEmpty()) {
+                dealWithRingsInParagraph(originalDocument);
+            }
+            else {
+                dealWithRingsInTable(originalDocument);
             }
 
             for (Ring ring : ringList) {
                 nameToTimesScore(ring);
                 if (sortedHashMap.containsKey(ring.getName())) {
                     sortedHashMap.put(ring.getName(), sortedHashMap.get(ring.getName()) + 1);
-                } else {
+                }
+                else {
                     sortedHashMap.put(ring.getName(), 1);
                 }
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
         return new ReaderResult(errorsList, nameToTimes, sortedHashMap);
@@ -69,96 +68,91 @@ public class RingReader {
     }
 
     private void dealWithRingsInParagraph(XWPFDocument document) {
-        boolean skipFirstNames = false;
+        boolean foundFirstRing  = false;
         Pattern pattern = Pattern.compile(".*\\p{L}.*");
         for (XWPFParagraph paragraph : document.getParagraphs()) {
             String sentence = paragraph.getText();
             String[] words = sentence.split("\\s+");
-            if (skipFirstNames) {
+
+            if (foundFirstRing) {
                 validateRing(sentence, words);
             }
             else {
-                if (pattern.matcher(sentence).matches()) {
-                    if (!isValidDouble(words[0]))
-                    {
-                        continue;
-                    }
-                    else {
-                        validateRing(sentence, words);
-                        skipFirstNames = true;
-                    }
+                if (pattern.matcher(sentence).matches() && isValidTimeCode(words[0])) {
+                    foundFirstRing  = true;
+                    validateRing(sentence, words);
                 }
             }
         }
     }
 
     private void dealWithRingsInTable(XWPFDocument document){
-        boolean checkTheHeader = false;
+        boolean skipTheHeader = false;
         for (XWPFTable table : document.getTables()) {
             for (XWPFTableRow row : table.getRows()) {
                 List<XWPFTableCell> cells = row.getTableCells();
-                if (cells.size() == 3) {
-                    String time = cells.get(0). getText().trim();
-                    String name = cells.get(1). getText().trim();
-                    String phrase = cells.get(2). getText().trim();
-                    if (!checkTheHeader) {
-                        if (time.equals("ТАЙМ-КОД") && name.equals("ПЕРСОНАЖ") && phrase.equals("ТЕКСТ")){
-                            checkTheHeader = true;
-                            continue;
-                        }
-                    }
-                    if (!checkTheHeader) {
-                        globalExceptionHandler.handleException(new TheHeaderTableException(ExceptionMessage.INCORRECT_TABLE_HANDLER.getMessage()));
-                        return;
-                    }
-                    name = name.split("\\(")[0];
-                    String sentence = time + " " + name + "\t" + phrase;
-                    String[] words = sentence.split("\\s+");
-
-                    if (time.isEmpty() || name.isEmpty() || phrase.isEmpty()) {
-                        errorsList.add("The cell is empty in the sentence: " + sentence);
+                if (cells.size() != 3) {
+                    writeError("The row must have 3 cells" + row);
+                    continue;
+                }
+                String time = cells.get(0). getText().trim();
+                String name = cells.get(1). getText().trim();
+                String phrase = cells.get(2). getText().trim();
+                if (!skipTheHeader) {
+                    if (time.equals("ТАЙМ-КОД") && name.equals("ПЕРСОНАЖ") && phrase.equals("ТЕКСТ")){
+                        skipTheHeader = true;
                         continue;
                     }
-                    validateRing(sentence, words);
+                    globalExceptionHandler.handleException(new TheHeaderTableException(ExceptionMessage.INCORRECT_TABLE_HANDLER.getMessage()));
+                    return;
                 }
-                else {
-                    writeError("The row must have 3 cells" + row);
+
+                if (time.isEmpty() || name.isEmpty() || phrase.isEmpty()) {
+                    errorsList.add("The cell is empty in the sentence: " + time + " " + name + "\t" + phrase);
+                    continue;
                 }
+
+                name = name.split("\\(")[0];
+                String sentence = time + " " + name + "\t" + phrase;
+                String[] words = sentence.split("\\s+");
+                validateRing(sentence, words);
             }
         }
     }
 
     private void validateRing(String sentence, String[] words){
-        if (words.length > 0){
-            if (words.length >= MIN_WORDS_TO_RING){
-                boolean isValidDouble = isValidDouble(words[0]);
-                Pattern pattern = Pattern.compile("^[\\p{Lu}\\p{N}\\-_/.;:,]+$");
-                Matcher matcher = pattern.matcher(words[1]);
-                boolean hasTheName = matcher.matches();
+        if (words.length < MIN_WORDS_TO_RING){
+            return;
+        }
+        boolean isValidTimeCode = isValidTimeCode(words[0]);
+        boolean isValidName = isValidName(words[1]);
 
-                if (isValidDouble && hasTheName){
-                    int errorsListCount = errorsList.size();
-                    words[1] = createTheName(sentence);
-                    if (errorsList.size() - 1 != errorsListCount){
-                        Ring ring = new Ring(words[0], words[1], words[2]);
-                        ringList.add(ring);
-                       // log.info("The ring was added : {} in the sentence: {}", words[1], sentence);
-                    }
-                } else if (!isValidDouble && hasTheName) {
-                    // if the sentence do not have a time code but have a Name
-                    if (words[1].length() > 1){
-                        writeError("The sentence does not have a time code: " + "in the sentence: " + sentence);
-                    }
-                }
-                else if (isValidDouble && !hasTheName){
-                    // if the sentence has a time code but do not have a time code
-                     writeError("The sentence does not have a proper Name: " + words[1] + "in the sentence: " + sentence);
-                }
-            }
+        if (!isValidTimeCode && isValidName) {
+           // if the sentence do not have a time code but have a Name
+           if (words[1].length() > 1){
+               writeError("The sentence does not have a time code: " + "in the sentence: " + sentence);
+           }
+           return;
+        }
+        else if (isValidTimeCode && !isValidName){
+            // if the sentence has a time code but do not have a time code
+            writeError("The sentence does not have a proper Name: " + words[1] + "in the sentence: " + sentence);
+            return;
+        }
+        else if (!isValidTimeCode) {
+            return;
+        }
+
+        int errorsListCount = errorsList.size();
+        words[1] = createName(sentence);
+        if (errorsList.size() == errorsListCount){
+            Ring ring = new Ring(words[0], words[1], words[2]);
+            ringList.add(ring);
+            // log.info("The ring was added : {} in the sentence: {}", words[1], sentence);
         }
     }
 
-    private String createTheName(String word1) {
+    private String createName(String word1) {
         String result;
         String word = word1.replaceFirst("^\\s*\\S+", "").trim();
         if (word.contains("\t")){
@@ -177,16 +171,24 @@ public class RingReader {
         errorsList.add(error);
     }
 
-    private static boolean isValidDouble(String str) {
-        if (!str.matches("\\d{2}[:;,\\.]\\d{2}")) {
+    private boolean isValidTimeCode(String str) {
+        if (!str.matches("\\d{2}[:;,.]\\d{2}")) {
             return false;
         }
-        String standardizedStr = str.replace(':', '.').replace(',', '.').replace(';', '.');
+        String standardizedStr = str.replace(':', '.')
+                                    .replace(',', '.')
+                                    .replace(';', '.');
         try {
             Double.parseDouble(standardizedStr);
             return true;
-        } catch (NumberFormatException e) {
+        }
+        catch (NumberFormatException e) {
             return false;
         }
+    }
+    private boolean isValidName(String name){
+        Pattern pattern = Pattern.compile("^[\\p{Lu}\\p{N}\\-_/.;:,]+$");
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
     }
 }
